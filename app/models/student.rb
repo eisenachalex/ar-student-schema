2bad4a9ba890f0c9419ea0f5124dd9f59eeb7e66
Alex Eisenach <hoopmiester@Alexs-MacBook-Pro-3.local>
2013-10-05 14:15:48 -0500

    answers

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,25 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+
+	validates :email, :format => { :with => /.*@.*\..{2,}/ }
+	validates :email, :uniqueness => true
+	validate :check_age
+	validate :phone, :format => { :with => /\D*(\d\D*){10}/ }
+	
+	def name
+	 p self.first_name + ' ' + self.last_name
+	end
+	
+	def age
+    return 0 unless birthday
+    Time.now.year - self.birthday.year
+ 	end
+ 	
+ 	def check_age
+ 		if age < 5
+ 		errors.add(:birthday, 'too young')
+ 		end
+ 	end
 end
+
